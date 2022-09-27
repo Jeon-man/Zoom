5994a6d93da04729486169123a8300a3b2464a2f
Jeon-man <gusals236@gmail.com>
2022-09-27 16:13:30 +0900

    Open room check

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -57,3 +57,16 @@ socket.on("bye", (user) => {
 });
 
 socket.on("new_message", addMessage);
+
+socket.on("room_change", (rooms) => {
+  const roomList = welcome.querySelector("#roomList");
+  roomList.innerHTML = "";
+  if (rooms.length === 0) {
+    return;
+  }
+  rooms.forEach((room) => {
+    const li = document.createElement("li");
+    li.innerText = room;
+    roomList.append(room);
+  });
+});
--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ function publicRooms() {
   } = wsServer;
   const publicRooms = [];
   rooms.forEach((_, key) => {
-    if (id.get(key) === undefined) {
+    if (sids.get(key) === undefined) {
       publicRooms.push(key);
     }
   });
@@ -44,12 +44,17 @@ wsServer.on("connection", (socket) => {
     socket.join(roomName);
     done();
     socket.to(roomName).emit("welcome", socket.nickname);
+    wsServer.sockets.emit("room_change", publicRooms());
   });
 
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) =>
       socket.to(room).emit("bye", socket.nickname)
     );
+  });
+
+  socket.on("disconnect", () => {
+    wsServer.sockets.emit("room_change", publicRooms());
   });
 
   socket.on("new_message", (msg, room, done) => {
